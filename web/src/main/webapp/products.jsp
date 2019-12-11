a6b5564d5e76d5f54b49bcf2622fc61d2a3675a2
Alexandra Szabó <szaboalexandra@gmail.com>
2019-12-11 10:59:02 +0100

    master: 4.feladatsor befejezve

--- web/src/main/webapp/products.jsp
+++ web/src/main/webapp/products.jsp
@@ -8,18 +8,19 @@
 	<table>
 		<c:forEach items="${sessionScope.products}" var="item">
 			<tr>
-				<td><c:out value="${item.name}" /></td>
-				<td><c:out value="${item.category}" /></td>
-				<td><c:out value="${item.unit}" /></td>
-				<td><c:out value="${item.purchasePrice}" /></td>
-				<td><c:out value="${item.salePrice}" /></td>
-				<td><c:out value="${item.description}" /></td>
+				<td class="name"><c:out value="${item.name}" /></td>
+				<td class="category"><c:out value="${item.category}" /></td>
+				<td class="unit"><c:out value="${item.unit}" /></td>
+				<td class="purchasePrice"><c:out value="${item.purchasePrice}" /></td>
+				<td class="salePrice"><c:out value="${item.salePrice}" /></td>
+				<td class="description"><c:out value="${item.description}" /></td>
 			</tr>
 		</c:forEach>
 	</table>
-	<form id="productsform" action="/warehouse/api/ProductService/getProducts" method="get">
-		<button id="refreshbutton" type="submit">Refresh</button>
-	</form>
+	
+		<button id="refreshbutton" type="submit" onclick="callServlet()">Refresh</button>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="resources/scripts/products.js"></script>
 </body>
 </html>
--- web/src/main/webapp/resources/scripts/products.js
+++ web/src/main/webapp/resources/scripts/products.js
@@ -1,30 +1,44 @@
-/*function callServlet() {
-	$('#productsform').submit(function(event) {
+function callServlet() {
+	
 
 		event.preventDefault();
-		var $form = $(this);
+	
 
 		$.ajax({
-			method : $form.attr("method"),
-			url : $form.attr("action"),
-			data : {
-				username : $('#inputUser').val(),
-				password : $('#inputPassword').val()
-			},
+			type: "GET",   
+	        url: "api/ProductService/getProducts",
+	        datatype: "json",
+
 			success : function(result) {
-
-				if (result["result"]) {
-					console.log("Üdvözöljük!");
-					location.replace("/warehouse/secured/profil.html");
-				} else {
-					console.log("Érvénytelen belépési adatok!");
-				}
+				document.write("<table>");
+				for (i = 0; i < 3; i++){
+					document.write("<tr>");
+					
+					document.write("<td>",result[i].name,"</td>");
+					document.write("<td>",result[i].category,"</td>");
+					document.write("<td>",result[i].unit,"</td>");
+					document.write("<td>",result[i].purchasePrice,"</td>");
+					document.write("<td>",result[i].salePrice,"</td>");
+					document.write("<td>",result[i].description,"</td>");
+					
+					document.write("</tr>");
+				}document.write("</table>");
+					
+//						$(".name").html(result[i].name),
+//						$(".category").html(result[i].category),
+//						$(".unit").html(result[i].unit),
+//						$(".purchasePrice").html(result[i].purchasePrice),
+//						$(".salePrice").html(result[i].salePrice),
+//						$(".description").html(result[i].description)
+//						
+//						//document.write(result[i].name);
+					
+				
 			},
-			failed : function() {
-
-				console.log("failed");
+			error : function(e) {
+				console.log("Error");
 			}
 
 		});
-	});
-}*/
+	
+}
